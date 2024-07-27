502eea2b467f93719194dd286b38209a99983529
Dr. Anatolii Tcai <136642858+HereIsAnatolii@users.noreply.github.com>
2024-07-27 12:34:52 +0200

    Update plecsgui.py

--- plecsgui.py
+++ plecsgui.py
@@ -1,13 +1,9 @@
-import subprocess
-import xmlrpc.client as xml
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-import re
-
 class PlecsGUI(tk.Tk):
     def __init__(self,title):
+        ''' title = "The title of your GUI window, the next two commands one needs to use are: 
+            \nadd_block(); 
+            \nrun()
+        '''
         # ensure that the inheritance works properly
         super().__init__()
         
@@ -40,7 +36,7 @@ class PlecsGUI(tk.Tk):
         # run
     
     def add_block(self,**kwargs):
-        ''' title, param (for plecs), multiplier, value, steps = (), limits = (), param'''
+        ''' title, param (that will be used in plecs), multiplier, value, steps : (small step,big step), limits : (min,max)'''
         if 'multiplier' not in kwargs:
             mul = 1
         else:
@@ -60,23 +56,34 @@ class PlecsGUI(tk.Tk):
         # Increment every time another block is added
         self.blocks +=1
         # Implement multiplier later
+        
     def run(self):
-        n_rows = (self.blocks)//5 + 1
+        n_rows = (self.blocks)//5 + 2
         n_cols = self.blocks//n_rows
         
         print(f"number of blocks {self.blocks}; number of cols {n_cols}; number of rows {n_rows}")
         self.size = {'x':146*(n_cols+1),'y':110*(n_rows)}
-        self.geometry(f"{self.size['x']}x{self.size['y']}")
-
+        self.params = []            
+        self.limits = []
+        self.muls = []
         i = 0
         for col in range(1,n_cols+1):
             for row in range(n_rows):
                 try:
                     self.menu[i].grid(column=col,row=row)
+                    self.params.append(self.menu[i].args['param'])
+                    self.limits.append(self.menu[i].args['limits'])
+                    self.muls.append(self.menu[i].args['multiplier'])
                 except IndexError:
                     print('Index error for menu')
                 i += 1
+        print(self.params)
+        self.loop = LoopSim(self,self.controller,self.params,self.limits,self.muls)
+        self.loop.grid(column=0,row=1)
+        self.geometry(f"{self.size['x']}x{int(self.size['y']+i/10)}")
+        
         self.mainloop()
+        
         
     def on_closing(self):
         # Additional functions to run before closing the GUI
@@ -102,7 +109,18 @@ class Controller:
         
     def get_shared_var(self):
         return self.shared_var
-    
+    def set_Nsims(self,Nsims):
+        self.Nsims = Nsims
+    def get_Nsims(self):
+        return self.Nsims
+    def set_params(self,param):
+        self.param = param
+    def get_params(self):
+        return self.param
+    def set_dict(self,dict_minmax):
+        self.dict_minmax = dict_minmax
+    def get_dict(self):
+        return self.dict_minmax
 # recreate ttk Frame method as a class
 class Opener(ttk.Frame):
     def __init__(self,parent,controller,**kwargs):
@@ -138,7 +156,44 @@ class Opener(ttk.Frame):
         close_button.pack(side=tk.RIGHT)
         
     def simulate(self):
-        self.plecs.simulate(self.model)
+        Nsims = int(self.controller.get_Nsims())
+        if Nsims > 1:
+            for i in range(Nsims):
+                if i == 0:
+                    try:
+                        self.plecs.scope(self.model+'/Scope','ClearTraces')
+                    except:
+                        print("Scope 'Scope' is not found")
+                # CHANGE THE PARAMETER
+                WhatToChange = self.controller.get_params()
+                print(f"{WhatToChange} will be looped")
+                input_string = self.plecs.get(self.model,'InitializationCommands')
+                re_str = WhatToChange + '.*?;'
+                
+                min_val = self.controller.get_dict()[WhatToChange][0]
+                max_val = self.controller.get_dict()[WhatToChange][1]
+                print(f"min_val = {min_val}, max_val = {max_val}")
+                values = np.linspace(min_val,max_val,Nsims)
+                
+                command = re.sub(f'^{re_str}',f"{WhatToChange} = {values[i]};",input_string,flags=re.MULTILINE)
+                
+                # update the current command with the new value
+                if input_string == command:
+                    command += f"\n{WhatToChange} = {values[i]}*{self.args['multiplier']};"
+                self.plecs.set(self.model,'InitializationCommands',command)
+                self.plecs.simulate(self.model)
+                
+                try:
+                    self.plecs.scope(self.model+'/Scope','HoldTrace',f"{WhatToChange} = {values[i]}")
+                except:
+                    print("Scope 'Scope' is not found")
+        else:
+            try:
+                self.plecs.scope(self.model+'/Scope','ClearTraces')
+            except:
+                print("Scope 'Scope' is not found")
+            self.plecs.simulate(self.model)
+            
     def close_file(self):
         self.controller.update_shared_var("EMPTY")
         self.plecs.close(self.model)
@@ -168,6 +223,98 @@ class Opener(ttk.Frame):
         print(f"{self.file} is loaded")
         self.frame_entry.insert(0,f"{self.file}")
         
+class LoopSim(ttk.Frame):
+    def __init__(self,parent,controller,params,limits,mul):
+    # create a block with RadioButtons which determine
+    # the loop-simulation
+        super().__init__(parent)
+        self.params = params
+        self.mul = mul
+        self.limits = limits
+        print(self.limits)
+        for i, limit in enumerate(self.limits):
+            self.limits[i] = [val*self.mul[i] for val in limit]
+#           self.limits[i] = limits[i]*self.mul[i]#*self.mul
+            
+        print(self.limits)
+        self.controller = controller
+        self.create_block()
+        self.controller.set_Nsims(1)
+        
+    def create_block(self):
+        main_frame = ttk.Frame(self, borderwidth=5, relief="solid")
+        title_frame = ttk.Frame(main_frame,
+                               borderwidth=1, relief="solid")
+        action_frame = ttk.Frame(main_frame)
+        
+        self.entry_var = tk.StringVar(value=1)
+        self.entry_var.trace_add("write", self.on_entry_change)
+        label = ttk.Label(title_frame,text='Number of Simulations')
+
+#        self.entry_var = tk.StringVar()
+#        self.entry_var.trace_add("write", self.on_entry_change)
+#        self.entry_var.set(f"{self.args['value']}")
+        self.frame_entry = ttk.Entry(action_frame, textvariable=self.entry_var)
+        
+#        self.frame_entry.insert(0,self.args['value'])
+        
+        button_up = ttk.Button(action_frame,text='>',width=9,command=self.increase)
+        button_dn = ttk.Button(action_frame,text='<',width=9,command=self.decrease)
+        self.choice = []
+        self.dict_params = {}
+        self.var = tk.StringVar(value=self.params[0])  # Set default value to "Option 1"
+        for i in range(len(self.params)):
+            self.choice.append( tk.Radiobutton(action_frame, variable=self.var, text=self.params[i], value=self.params[i],command=self.set_choice) )
+            self.dict_params[self.params[i]] = self.limits[i]
+            self.dict_params[self.params[i]] = self.limits[i]
+        self.controller.set_dict(self.dict_params)
+        main_frame.pack()
+        title_frame.pack(fill='x')
+        label.pack()
+        
+        action_frame.pack(fill='x')
+        self.frame_entry.pack(fill='x')
+        for i in range(len(self.params)):
+            self.choice[i].pack(side=tk.BOTTOM)
+            
+        button_dn.pack(side=tk.LEFT)
+        button_up.pack(side=tk.LEFT)
+    def decrease(self):
+        self.modify_entry(operand='-')
+    def increase(self):
+        self.modify_entry(operand='+')
+    def on_entry_change(self, *args):
+        # This method is called whenever the content of the entry widget changes
+        model_name = self.controller.get_shared_var()
+        if model_name == 'EMPTY':
+            print('PLECS model not loaded')
+        else:
+            value = self.entry_var.get().split('.')[0]
+            self.controller.set_Nsims(value)
+            self.controller.set_params(self.var.get())
+            print(self.dict_params[self.var.get()])
+    def set_choice(self):
+        selected_value = self.var.get()
+        print(f"Selected option: {selected_value}")
+        self.controller.set_params(self.var.get())
+        print(self.dict_params[self.var.get()])
+        
+    def modify_entry(self,operand='+'):
+        current_value = float(self.frame_entry.get())
+        
+        if operand == '+':
+            new_value = current_value + 1
+        elif operand == '-':
+            new_value = current_value - 1
+            
+        if new_value <= 1:
+            new_value = 1
+        try:
+            self.frame_entry.delete(0,tk.END)
+            self.frame_entry.insert(0,new_value)
+        except ValueError:
+            self.frame_entry.insert(0,current_value)
+            print('ValueError')
 # recreate ttk Frame method as a class
 class Block(ttk.Frame):
     def __init__(self,parent,controller,**kwargs):
@@ -271,4 +418,3 @@ class Block(ttk.Frame):
         except ValueError:
             self.frame_entry.insert(0,current_value)
             print('ValueError')
-         
